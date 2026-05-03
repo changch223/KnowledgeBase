454cdb664d759bc00c5c8e2b1ef8eddc9c8edff5
Chia-Wei <chang.ch23@gmail.com>
2026-05-04 08:28:05 +0900

    Initial Commit

---
+++ KnowledgeTreeTests/KnowledgeTreeTests.swift
@@ -0,0 +1,18 @@
+//
+//  KnowledgeTreeTests.swift
+//  KnowledgeTreeTests
+//
+//  Created by chang chiawei on 2026/05/04.
+//
+
+import Testing
+
+struct KnowledgeTreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
